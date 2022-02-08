baabb1876c34c247529e5d8d10042574b4644812
Karlo Hurtado <808khz@gmail.com>
2022-02-08 16:55:37 -0600

    Changed project base
    Changed project base and added some new packages.

--- Assets/Scripts/Scriptables/SceneDialogues.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu]
-public class SceneDialogues : ScriptableObject
-{
-    public TextAsset[] dialoguesForScene;
-}
